e80bb3b338b36ecba45f56e8c0e17141647c92f2
Zechariah hounwanou <zechariahhounwanou@gmail.com>
2023-03-18 15:19:19 +0100

    Added Active and completed Tabs

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,17 +8,33 @@ interface TodoListProps {
 }
 const TodoList = ({ todoList, setTodoList }: TodoListProps) => {
   return (
-    <div className="todo-list">
-      {todoList.map((todo) => {
-        return (
-          <SingleTodo
-            todoList={todoList}
-            setTodoList={setTodoList}
-            todo={todo}
-            key={todo.todoId}
-          />
-        );
-      })}
+    <div className="todo-container">
+      <div className="todo-list">
+        <span className="todo-heading">Active Task</span>
+        {todoList.map((todo) => {
+          return (
+            <SingleTodo
+              todo={todo}
+              todoList={todoList}
+              setTodoList={setTodoList}
+              key={todo.todoId}
+            />
+          );
+        })}
+      </div>
+      <div className="todo-list completed-todo">
+        <span className="todo-heading">Completed Task</span>
+        {todoList.map((todo) => {
+          return (
+            <SingleTodo
+              todo={todo}
+              todoList={todoList}
+              setTodoList={setTodoList}
+              key={todo.todoId}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
